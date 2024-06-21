589d434fd28d64c88d773fa2199eca5560a475b5
HydroSulphide <56787661+HydroSulphide@users.noreply.github.com>
2024-06-21 22:06:37 +0200

    Added gummi stock addresses to GlobalConstants.lua and cheat table. Added example script for setting amount of specific gummi in stock.

---
+++ Lua/ExampleScripts/SetGummiAmountInStock.lua
@@ -0,0 +1,28 @@
+LUAGUI_NAME = "SetGummiAmountInStock"
+LUAGUI_AUTH = "HydroSulphide"
+LUAGUI_DESC = "Amount of specified gummi in stock will be set to a given fixed value."
+
+require("GlobalConstants")
+
+local isSteamVersion = 0x232DD64
+
+local gummiStartAddress = 0x2DF51DC
+local amount = 99
+
+
+
+-- Initalizing the script
+function _OnInit()
+	if ENGINE_TYPE == "BACKEND" then
+		ConsolePrint("SetGummiAmountInStock - installed")
+	end
+end
+
+
+
+function _OnFrame()
+	if ReadInt(isSteamVersion) == 0x41455453 then
+		-- Set the Byte of the address of the item in stock to the desired amount
+    	WriteByte(gummiStartAddress + GlobalConstants:GetGummi().ULTIMA_G, amount)
+	end
+end
